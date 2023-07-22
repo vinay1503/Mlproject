6110a5d99304ba607fb8e5d843e1e2211bb1f935
Vinay Maheshwar <m.vinay15@gmail.com>
2023-07-22 20:30:04 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 from setuptools import find_packages,setup
-from typing import List 
+from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace ("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-
-    return requirements
     
+    return requirements
+
 setup(
 name='mlproject',
 version='0.0.1',
 author='Vinay',
-author_email='vinay.maheshwar@gmail.com',
+author_email='m.vinay15@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
-
 
 )
